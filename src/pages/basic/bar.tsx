e18baa88f48cc20ab80ca4ce81df861be440f6e7
zhanghao <hzhang28@trip.com>
2023-05-09 14:06:36 +0800

    init

---
+++ src/pages/basic/bar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+
+
+export default function MyReactPage() {
+  return (
+    <Layout>
+      <h1>My React page</h1>
+      <p>This is a React page嗡嗡嗡嗡嗡嗡</p>
+    </Layout>
+  );
+}
